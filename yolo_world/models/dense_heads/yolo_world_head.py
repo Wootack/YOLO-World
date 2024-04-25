95c5dca2978afe7879b37698e1af65f8342f9527
Wootack Kim <wootack.kim@hcs.snu.ac.kr>
2024-04-25 15:00:41 +0900

    feat: more records on head functions

--- yolo_world/models/dense_heads/yolo_world_head.py
+++ yolo_world/models/dense_heads/yolo_world_head.py
@@ -525,133 +525,141 @@ class YOLOWorldHead(YOLOv8Head):
             - bboxes (Tensor): Has a shape (num_instances, 4),
               the last dimension 4 arrange as (x1, y1, x2, y2).
         """
-        assert len(cls_scores) == len(bbox_preds)
-        if objectnesses is None:
-            with_objectnesses = False
-        else:
-            with_objectnesses = True
-            assert len(cls_scores) == len(objectnesses)
+        with record_function("YW-predict-bbox-boxhead-init"):
+            assert len(cls_scores) == len(bbox_preds)
+            if objectnesses is None:
+                with_objectnesses = False
+            else:
+                with_objectnesses = True
+                assert len(cls_scores) == len(objectnesses)
 
-        cfg = self.test_cfg if cfg is None else cfg
-        cfg = copy.deepcopy(cfg)
+            cfg = self.test_cfg if cfg is None else cfg
+            cfg = copy.deepcopy(cfg)
 
-        multi_label = cfg.multi_label
-        multi_label &= self.num_classes > 1
-        cfg.multi_label = multi_label
+            multi_label = cfg.multi_label
+            multi_label &= self.num_classes > 1
+            cfg.multi_label = multi_label
 
-        num_imgs = len(batch_img_metas)
-        featmap_sizes = [cls_score.shape[2:] for cls_score in cls_scores]
+            num_imgs = len(batch_img_metas)
+            featmap_sizes = [cls_score.shape[2:] for cls_score in cls_scores]
 
-        # If the shape does not change, use the previous mlvl_priors
-        if featmap_sizes != self.featmap_sizes:
-            self.mlvl_priors = self.prior_generator.grid_priors(
-                featmap_sizes,
-                dtype=cls_scores[0].dtype,
-                device=cls_scores[0].device)
-            self.featmap_sizes = featmap_sizes
-        flatten_priors = torch.cat(self.mlvl_priors)
+            # If the shape does not change, use the previous mlvl_priors
+            if featmap_sizes != self.featmap_sizes:
+                self.mlvl_priors = self.prior_generator.grid_priors(
+                    featmap_sizes,
+                    dtype=cls_scores[0].dtype,
+                    device=cls_scores[0].device)
+                self.featmap_sizes = featmap_sizes
+            flatten_priors = torch.cat(self.mlvl_priors)
 
-        mlvl_strides = [
-            flatten_priors.new_full(
-                (featmap_size.numel() * self.num_base_priors, ), stride) for
-            featmap_size, stride in zip(featmap_sizes, self.featmap_strides)
-        ]
-        flatten_stride = torch.cat(mlvl_strides)
-
-        # flatten cls_scores, bbox_preds and objectness
-        flatten_cls_scores = [
-            cls_score.permute(0, 2, 3, 1).reshape(num_imgs, -1,
-                                                  self.num_classes)
-            for cls_score in cls_scores
-        ]
-        flatten_bbox_preds = [
-            bbox_pred.permute(0, 2, 3, 1).reshape(num_imgs, -1, 4)
-            for bbox_pred in bbox_preds
-        ]
-
-        flatten_cls_scores = torch.cat(flatten_cls_scores, dim=1).sigmoid()
-        flatten_bbox_preds = torch.cat(flatten_bbox_preds, dim=1)
-        flatten_decoded_bboxes = self.bbox_coder.decode(
-            flatten_priors[None], flatten_bbox_preds, flatten_stride)
-
-        if with_objectnesses:
-            flatten_objectness = [
-                objectness.permute(0, 2, 3, 1).reshape(num_imgs, -1)
-                for objectness in objectnesses
+            mlvl_strides = [
+                flatten_priors.new_full(
+                    (featmap_size.numel() * self.num_base_priors, ), stride) for
+                featmap_size, stride in zip(featmap_sizes, self.featmap_strides)
             ]
-            flatten_objectness = torch.cat(flatten_objectness, dim=1).sigmoid()
-        else:
-            flatten_objectness = [None for _ in range(num_imgs)]
+            flatten_stride = torch.cat(mlvl_strides)
+
+        with record_function("YW-predict-bbox-boxhead-scores"):
+            # flatten cls_scores, bbox_preds and objectness
+            flatten_cls_scores = [
+                cls_score.permute(0, 2, 3, 1).reshape(num_imgs, -1,
+                                                      self.num_classes)
+                for cls_score in cls_scores
+            ]
+            flatten_bbox_preds = [
+                bbox_pred.permute(0, 2, 3, 1).reshape(num_imgs, -1, 4)
+                for bbox_pred in bbox_preds
+            ]
+
+            flatten_cls_scores = torch.cat(flatten_cls_scores, dim=1).sigmoid()
+            flatten_bbox_preds = torch.cat(flatten_bbox_preds, dim=1)
+            flatten_decoded_bboxes = self.bbox_coder.decode(
+                flatten_priors[None], flatten_bbox_preds, flatten_stride)
+
+            if with_objectnesses:
+                flatten_objectness = [
+                    objectness.permute(0, 2, 3, 1).reshape(num_imgs, -1)
+                    for objectness in objectnesses
+                ]
+                flatten_objectness = torch.cat(flatten_objectness, dim=1).sigmoid()
+            else:
+                flatten_objectness = [None for _ in range(num_imgs)]
         # 8400
         # print(flatten_cls_scores.shape)
         results_list = []
-        for (bboxes, scores, objectness,
-             img_meta) in zip(flatten_decoded_bboxes, flatten_cls_scores,
-                              flatten_objectness, batch_img_metas):
-            ori_shape = img_meta['ori_shape']
-            scale_factor = img_meta['scale_factor']
-            if 'pad_param' in img_meta:
-                pad_param = img_meta['pad_param']
-            else:
-                pad_param = None
+        with record_function("YW-predict-bbox-boxhead-core"):
+            for (bboxes, scores, objectness,
+                 img_meta) in zip(flatten_decoded_bboxes, flatten_cls_scores,
+                                  flatten_objectness, batch_img_metas):
+                ori_shape = img_meta['ori_shape']
+                scale_factor = img_meta['scale_factor']
+                if 'pad_param' in img_meta:
+                    pad_param = img_meta['pad_param']
+                else:
+                    pad_param = None
 
-            score_thr = cfg.get('score_thr', -1)
-            # yolox_style does not require the following operations
-            if objectness is not None and score_thr > 0 and not cfg.get(
-                    'yolox_style', False):
-                conf_inds = objectness > score_thr
-                bboxes = bboxes[conf_inds, :]
-                scores = scores[conf_inds, :]
-                objectness = objectness[conf_inds]
+                score_thr = cfg.get('score_thr', -1)
+                score_thr = 0.5
+                # yolox_style does not require the following operations
+                if objectness is not None and score_thr > 0 and not cfg.get(
+                        'yolox_style', False):
+                    conf_inds = objectness > score_thr
+                    bboxes = bboxes[conf_inds, :]
+                    scores = scores[conf_inds, :]
+                    objectness = objectness[conf_inds]
 
-            if objectness is not None:
-                # conf = obj_conf * cls_conf
-                scores *= objectness[:, None]
+                if objectness is not None:
+                    # conf = obj_conf * cls_conf
+                    scores *= objectness[:, None]
 
-            if scores.shape[0] == 0:
-                empty_results = InstanceData()
-                empty_results.bboxes = bboxes
-                empty_results.scores = scores[:, 0]
-                empty_results.labels = scores[:, 0].int()
-                results_list.append(empty_results)
-                continue
+                if scores.shape[0] == 0:
+                    empty_results = InstanceData()
+                    empty_results.bboxes = bboxes
+                    empty_results.scores = scores[:, 0]
+                    empty_results.labels = scores[:, 0].int()
+                    results_list.append(empty_results)
+                    continue
 
-            nms_pre = cfg.get('nms_pre', 100000)
-            if cfg.multi_label is False:
-                scores, labels = scores.max(1, keepdim=True)
-                scores, _, keep_idxs, results = filter_scores_and_topk(
-                    scores,
-                    score_thr,
-                    nms_pre,
-                    results=dict(labels=labels[:, 0]))
-                labels = results['labels']
-            else:
-                scores, labels, keep_idxs, _ = filter_scores_and_topk(
-                    scores, score_thr, nms_pre)
+                nms_pre = cfg.get('nms_pre', 100000)
+                with record_function("YW-predict-bbox-boxhead-core-topk"):
+                    if cfg.multi_label is False:
+                        scores, labels = scores.max(1, keepdim=True)
+                        scores, _, keep_idxs, results = filter_scores_and_topk(
+                            scores,
+                            score_thr,
+                            nms_pre,
+                            results=dict(labels=labels[:, 0]))
+                        labels = results['labels']
+                    else:
+                        scores, labels, keep_idxs, _ = filter_scores_and_topk(
+                            scores, score_thr, nms_pre)
 
-            results = InstanceData(scores=scores,
-                                   labels=labels,
-                                   bboxes=bboxes[keep_idxs])
+                results = InstanceData(scores=scores,
+                                       labels=labels,
+                                       bboxes=bboxes[keep_idxs])
 
-            if rescale:
-                if pad_param is not None:
-                    results.bboxes -= results.bboxes.new_tensor([
-                        pad_param[2], pad_param[0], pad_param[2], pad_param[0]
-                    ])
-                results.bboxes /= results.bboxes.new_tensor(
-                    scale_factor).repeat((1, 2))
+                if rescale:
+                    with record_function("YW-predict-bbox-boxhead-core-rescale"):
+                        if pad_param is not None:
+                            results.bboxes -= results.bboxes.new_tensor([
+                                pad_param[2], pad_param[0], pad_param[2], pad_param[0]
+                            ])
+                        results.bboxes /= results.bboxes.new_tensor(
+                            scale_factor).repeat((1, 2))
 
-            if cfg.get('yolox_style', False):
-                # do not need max_per_img
-                cfg.max_per_img = len(results)
+                if cfg.get('yolox_style', False):
+                    # do not need max_per_img
+                    cfg.max_per_img = len(results)
 
-            results = self._bbox_post_process(results=results,
-                                              cfg=cfg,
-                                              rescale=False,
-                                              with_nms=with_nms,
-                                              img_meta=img_meta)
-            results.bboxes[:, 0::2].clamp_(0, ori_shape[1])
-            results.bboxes[:, 1::2].clamp_(0, ori_shape[0])
+                with record_function("YW-predict-bbox-boxhead-core-postprocess"):
+                    results = self._bbox_post_process(results=results,
+                                                      cfg=cfg,
+                                                      rescale=False,
+                                                      with_nms=with_nms,
+                                                      img_meta=img_meta)
+                with record_function("YW-predict-bbox-boxhead-core-clamp"):
+                    results.bboxes[:, 0::2].clamp_(0, ori_shape[1])
+                    results.bboxes[:, 1::2].clamp_(0, ori_shape[0])
 
-            results_list.append(results)
+                results_list.append(results)
         return results_list
